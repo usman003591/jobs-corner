24fc957e484d68756e598c2c901e8dc87abda189
usman003591 <usman003591@gmail.com>
2024-11-30 13:00:50 +0500

    Added more design elements

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
     <section>
-        <h1 class="font-bold text-4xl mb-5">Shape Your Future, Find the Perfect Job</h1>
-        <form class="flex items-center max-w-lg mx-auto">
+        <h1 class="font-bold text-4xl mb-7">Shape Your Future, Find the Perfect Job</h1>
+        <form class="flex items-center max-w-lg mx-auto mb-20">
             <div class="relative w-full">
                 <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
                     <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
@@ -13,7 +13,7 @@
                     </div>
                 </div>
                 <input type="text" id="voice-search"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    class="bg-transparent border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="Search Your Dream Job" />
                 <button type="submit"
                     class="absolute top-0 end-0 p-2.5 text-sm h-full text-white bg-blue-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
