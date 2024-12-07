89e84bb6724f7d8e1d6fedad3e31dad5c7dee7d9
usman003591 <usman003591@gmail.com>
2024-12-07 12:46:11 +0500

    Done some changes

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,14 +2,28 @@
 
 namespace App\Models;
 
+use App\Models\Tag;
 use App\Models\Employer;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Job extends Model
 {
     //Relationships
-    public function employer()
+    public function employer(): BelongsTo
     {
         return $this->belongsTo(Employer::class);
+    }
+
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+
+    public function tag(string $name): void
+    {
+        $tag = Tag::firstOrCreate(['name' => $name]);
+        $this->tags()->attach($tag);
     }
 }
