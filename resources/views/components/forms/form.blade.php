b45a459c90cb00deba2ef5aff27ccecb0fd88ab6
Malik Usman <usman003591@gmail.com>
2025-01-03 12:25:42 +0500

    Created some components

---
+++ resources/views/components/forms/form.blade.php
@@ -0,0 +1,1 @@
+<form {{ $attributes(['class' => 'max-w-2xl mx-auto space-y-6']) }}></form>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,15 @@
 <?php
 
-use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\JobController;
+use App\Http\Controllers\SessionController;
+use App\Http\Controllers\RegisteredUserController;
 
 Route::get('/', [JobController::class, 'index']);
+
+Route::get('/register', [RegisteredUserController::class, 'create']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
+
+Route::get('/login', [SessionController::class, 'create']);
+Route::post('/login', [SessionController::class, 'store']);
+Route::delete('/logout', [SessionController::class, 'destroy']);
