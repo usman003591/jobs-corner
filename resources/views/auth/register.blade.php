6fe6a0bbfd9af556d147c2c3372aebf4548066ad
Malik Usman <usman003591@gmail.com>
2025-01-15 13:00:12 +0500

    store function completed for users and employers

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,10 +1,19 @@
 <x-layout>
     <x-page-heading>Register</x-page-heading>
-    <x-forms.form method="POST" action="/register">
+    <x-forms.form method="POST" action="/register" enctype="multipart/form-data">
         <x-forms.input label="Name" name="name" />
         <x-forms.input label="Email" name="email" type="email" />
         <x-forms.input label="Password" name="password" type="password" />
         <x-forms.input label="Confirm Password" name="password_confirmation" type="password" />
-        <x-forms.button>Create Account</x-forms.button>
+        <x-forms.divider/>
+
+        <x-forms.input label="Employer Name" name="employer" />
+        <x-forms.input label="Employer Email" name="email" />
+        <x-forms.input label="Employer Address" name="address" />
+        <x-forms.input label="Employer Contact" name="contact" />
+        <x-forms.input label="Employer Logo" name="logo" type="file" />
+        <div class="text-right">
+            <x-forms.button>Create Account</x-forms.button>
+        </div>
     </x-forms.form>
 </x-layout>
